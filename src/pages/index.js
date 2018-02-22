d0a04fb57752ef001fe0bb3a9e1305ca7ad5752f
alex jenter <alex@attribute.ch>
2018-02-22 18:11:28 +0100

    Initial Commit

---
+++ src/pages/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    }
+  }
+  handleChange = event => {
+    this.setState({ text: event.target.value })
+  }
+  handleSend = event => {
+    let data = { root: this.state.text };
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:4040/save", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(data));
+  }
+  render() {
+    return (
+      <div>
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.text}
+          name="test"
+          cols="30"
+          rows="10"
+        >
+        </textarea>
+        <button onClick={this.handleSend}>send</button>
+      </div>
+    )
+  }
+}
+
+export default IndexPage
